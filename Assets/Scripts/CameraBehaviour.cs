18adb82badf6def07445c28e68c0c20875910af1
wesselvink <22092@ma-web.nl>
2017-11-28 15:10:23 +0100

    Camera Script toegevoegd

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public GameObject player;
+
+
+	private Vector3 offset;         
+
+
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+	}
+		
+	void Update () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
