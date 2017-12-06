09ae20106b036ab7cc65d77236a4ad9ac845824d
WolfFire404 <reillyvas9@live.nl>
2017-12-06 12:37:12 +0100

    Minimap helemaal gefixt qua script

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour 
+{
+	private MeshRenderer meshRenderer;
+	private bool CheckSwitcher = true;
+
+	void Start() 
+	{
+		meshRenderer = GetComponent<MeshRenderer> ();
+		StartCoroutine (Switcher ());
+	}
+	
+	IEnumerator Switcher()
+	{
+		while (CheckSwitcher) {
+			meshRenderer.enabled = !meshRenderer.enabled;
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
