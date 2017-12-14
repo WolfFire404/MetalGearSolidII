391f259041a680091b3ec0dff3cdbb779435cd07
WolfFire404 <reillyvas9@live.nl>
2017-12-14 10:49:52 +0100

    beginnen met in elkaar zetten

---
+++ Assets/Scripts/MinimapInputManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapInputManager : MonoBehaviour
+{
+
+    public float lookSpeed = 10;
+    public float moveSpeed;
+    private Vector3 curLoc;
+    private Vector3 prevLoc;
+
+    void Update()
+    {
+        InputListen();
+        if (curLoc != prevLoc)
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - prevLoc), Time.fixedDeltaTime * lookSpeed);
+    }
+
+    private void InputListen()
+    {
+        prevLoc = curLoc;
+        curLoc = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+            curLoc.z -= 1 * Time.fixedDeltaTime * moveSpeed;
+        if (Input.GetKey(KeyCode.D))
+            curLoc.z += 1 * Time.fixedDeltaTime * moveSpeed;
+        if (Input.GetKey(KeyCode.W))
+            curLoc.y += 1 * Time.fixedDeltaTime * moveSpeed;
+        if (Input.GetKey(KeyCode.S))
+            curLoc.y -= 1 * Time.fixedDeltaTime * moveSpeed;
+
+        transform.position = curLoc;
+    }
+}
