c73eef05985dcd69a45a29013825ec8080cddc96
ElgianniKolf <elgianni14@hotmail.com>
2017-12-22 20:55:02 +0100

    Added animations -alleen nog animaties toepassen

---
+++ Assets/Scripts/Player/AnimationManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    Animator anim;
+    int runHash = Animator.StringToHash("Run");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", move);
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetKeyDown(KeyCode.A) && stateInfo.nameHash== runStateHash)
+        {
+            anim.SetTrigger(runHash);
+        }
+    }
+}
